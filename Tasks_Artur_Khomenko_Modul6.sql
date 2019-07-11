479ff31b852e02f40dc1d8c79c7c11cd83bee6a9
KhomenkoArtur <Khomenkoartur2015@gmail.com>
2019-07-11 23:56:32 +0300

    Update Tasks_Artur_Khomenko_Modul6.sql

--- Tasks_Artur_Khomenko_Modul6.sql
+++ Tasks_Artur_Khomenko_Modul6.sql
@@ -2,21 +2,20 @@ Use Modul6;
 
 go
 
---1. Написати команду, яка повертає список продуктів, складений в алфавітному порядку міст де вони знаходяться
--- + порядковий номер деталі в списку (наскрізна нумерація для порядкового номера)
+--1
+
 
 Select productid,name, city,
 ROW_NUMBER() Over(Order by city ) as RowNumber
 from products
 
---2. Написати команду, яка повертає список продуктів, складений в алфавітному порядку міст де вони знаходяться
--- + порядковий номер в межах одного міста (відсортований за іменем продукту)
+--2
 
 Select productid,name, city,
 ROW_NUMBER() Over(PARTITION by city Order by name ) as RowNumber
 from products
 
---3. Використовуючи за основу попередній запит написати запит, який повертає міста з порядковим номером 1.
+--3
 
 Select *
 	from (Select productid,name, city,
@@ -25,8 +24,7 @@ Select *
 
 		Where RowNumber = 1
 
---4. Написати запит, який повертає список продуктів, деталей, їхні поставки, 
---загальну кількість поставок для кожного продукту і загальну кількість поставок для кожної деталі.
+--4
 Select s.productid, s.detailid, s.quantity,
 	SUM(s.quantity) Over(Partition by s.productid ) as all_quantity_per_prod,
 	SUM(s.quantity) Over(Partition by s.detailid ) as all_quantity_per_det
@@ -34,9 +32,7 @@ Select s.productid, s.detailid, s.quantity,
 		join [dbo].[supplies] as s ON p.productid = s.productid
 			join details as d ON s.detailid = d.detailid
 
---5. Організувати посторінковий вивід інформації з таблиці поставок, відсортований за  постачальниками,
---вивести записи з 10 по 15 запис, додатково вивести порядковий номер стрічки і загальну кількість записів 
---у таблиці поставок.
+--5
 
 Select *
 	from (Select *,
@@ -45,8 +41,7 @@ Select *
 		from [dbo].[supplies]) as a	
 Where RowNumber between 10 and 15
 
---6. Написати запит, що розраховує середню кількість елементів в поставці і виводить ті поставки,
--- де кількість елементів менше середньої.
+--6
 Select *
 	from (Select supplierid, detailid,productid,quantity,
 			AVG(quantity) over()as avg_quantity  
